63945170b3bede01891e84e2723e5e7849143edf
Siyami Avci <siyamiavci@gmail.com>
2017-05-15 22:23:52 -0700

    improve index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
-
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyAuWXUuk4qjunifQD3WsshLHhBUXNgfJcE';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
