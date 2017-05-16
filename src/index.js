8495473abca2cc0017f97cc5eb41a5fb87fa7a0f
Siyami Avci <siyamiavci@gmail.com>
2017-05-15 22:04:24 -0700

    add SearchBar Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyAuWXUuk4qjunifQD3WsshLHhBUXNgfJcE';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
